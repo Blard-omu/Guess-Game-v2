e9d171749603c3faf7ad4bdddc96ecb974fcf90f
Blard <peteromu76@gmail.com>
2023-05-17 21:34:30 +0100

    first commit

---
+++ scripts/counter.js
@@ -0,0 +1,40 @@
+// selecting DOM elements
+const result = document.querySelector('.result');
+const decrease = document.querySelector('.dec');
+const increase = document.querySelector('.inc');
+const reset = document.querySelector('.res');
+const display = document.querySelector('.display');
+// console.log(result);
+// console.log(reset);
+// console.log(decrease, increase);
+
+
+// For decrease
+decrease.addEventListener('click', ()=>{
+    result.innerHTML --;
+    handleColorChange()
+})
+
+// For increase
+increase.addEventListener('click', ()=>{
+    result.innerHTML ++;
+    handleColorChange()
+})
+
+// For reset
+reset.addEventListener('click', ()=>{
+    result.innerHTML = 0;
+    handleColorChange()
+})
+
+function handleColorChange(){
+    if(result.innerHTML < 0){
+        display.style.color = 'red';
+    }else if(result.innerHTML > 0){
+        display.style.color = 'green';
+    }else{
+        display.style.color = '#fff';
+    }
+    
+}
+
